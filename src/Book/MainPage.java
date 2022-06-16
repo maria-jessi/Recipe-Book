7bc563c5fc42a4745b99124dfc6ef0d37990b87b
Maria <maria35-737@diu.edu.bd>
2022-06-16 18:27:42 +0600

    window te main page add korci .main page title ar object create koreci

--- src/Book/MainPage.java
+++ src/Book/MainPage.java
@@ -3,9 +3,10 @@ package Book;
 import javax.swing.*;
 
 public class MainPage extends JPanel {
+    Title Header=new Title();
     public MainPage(){
         setLayout(null);
         setSize(1000,700);
-
+        add(Header);
     }
 }
--- src/Window.java
+++ src/Window.java
@@ -1,8 +1,13 @@
+import Book.MainPage;
+
 import javax.swing.*;
 
 public class Window extends JFrame {
+    MainPage HomePage=new MainPage();
+
     public Window(){
         setSize(1000,700);
+        setContentPane(HomePage);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
     }
