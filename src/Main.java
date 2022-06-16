e52980ca19e0c5fe38d537b53a0741641b2199b0
Maria <maria35-737@diu.edu.bd>
2022-06-16 16:47:30 +0600

    window created

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi Maria! Kemon aso?");
+        Window window=new Window();
 
     }
 }
---
+++ src/Window.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Window extends JFrame {
+    public Window(){
+        setSize(1000,700);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
