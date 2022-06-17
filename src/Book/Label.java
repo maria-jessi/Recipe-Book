119d658acfc2c6e9ea692bb7ae23ae9f922f5434
Maria <maria35-737@diu.edu.bd>
2022-06-17 21:25:20 +0600

    Label,ingredients,procedure ai gola create kora hoice

--- src/Book/Label.java
+++ src/Book/Label.java
@@ -5,9 +5,9 @@ import java.awt.*;
 
 public class Label extends JLabel {
     public Label(String text,int x ,int y){
-        setBounds(x,y,200,100);
+        setBounds(x,y,150,50);
         setText(text);
-        setFont(new Font("Serif", Font.BOLD, 20));
+        setFont(new Font("Serif", Font.BOLD, 22));
 
     }
 }
--- src/Book/MainPage.java
+++ src/Book/MainPage.java
@@ -8,17 +8,42 @@ public class MainPage extends JPanel {
     Title Header=new Title();
     Label label1=new Label("Name",30,80);
     ArrayList<String> foodName = new ArrayList<>();
+    Label label2=new Label("Ingredients",30,150);
+    ArrayList<String> foodIngredients = new ArrayList<>();
+    Label label3=new Label("Procedure",460,10);
+    ArrayList<String> foodProcedure = new ArrayList<>();
     JComboBox<String> foodList = new JComboBox<>();
+    JTextArea ingredientsText=new JTextArea();
+    JTextArea procedureText=new JTextArea();
+
+
 
     public MainPage(){
         setLayout(null);
         setBounds(0,0,1000,700);
 
+
         add(Header);
         add(label1);
+        add(label2);
+        add(label3);
         add(foodList);
+        add(ingredientsText);
+        add(procedureText);
 
-        foodList.setBounds(150,100,200,50);
+        foodList.setBounds(110,80,300,50);
         foodList.setFont(new Font("Serif", Font.BOLD, 20));
+        ingredientsText.setBounds(25,200,405,400);
+        procedureText.setBounds(455,65,510,570);
+
+
+        foodName.add("rice");
+        foodName.add("tomato curry");
+        foodName.add(1,"omelette");
+
+        for (int i = 0; i < foodName.size(); i++) {
+            foodList.addItem(foodName.get(i));
+
+        }
     }
 }
