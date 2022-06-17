166d4113f8359305963894962852c5d6e06294a3
Maria <maria35-737@diu.edu.bd>
2022-06-17 17:39:05 +0600

    foodList create koreci. foodList ar seeBounds and setFont create kora hoice.

--- src/Book/MainPage.java
+++ src/Book/MainPage.java
@@ -1,16 +1,24 @@
 package Book;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class MainPage extends JPanel {
     Title Header=new Title();
     Label label1=new Label("Name",30,80);
     ArrayList<String> foodName = new ArrayList<>();
+    JComboBox<String> foodList = new JComboBox<>();
+
     public MainPage(){
         setLayout(null);
         setBounds(0,0,1000,700);
+
         add(Header);
         add(label1);
+        add(foodList);
+
+        foodList.setBounds(150,100,200,50);
+        foodList.setFont(new Font("Serif", Font.BOLD, 20));
     }
 }
